96b2e4d581e1e057518e6d083a77d8ef4899a731
OlgaFofanova <fofanaya@yandex.ru>
2021-08-19 16:59:38 +0300

    верстка

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,16 +1,14 @@
 import React from 'react';
 import './Header.css';
-import logo from '../../images/logo.svg';
 import NavTab from '../NavTab/NavTab';
 import ButtonProfile from '../ButtonProfile/ButtonProfile';
 import ButtonCollMemu from '../ButtonCollMemu/ButtonCollMemu';
+import Logo from '../Logo/Logo';
 
 function Header() {
     return ( 
         <div className="header">
-        <a href="#" target="_blank">
-        <img className="logo" src={logo}  alt="Логотип"/> 
-        </a> 
+        <Logo />
         <div  className="header__nav">
             <NavTab />
             <ButtonProfile />
